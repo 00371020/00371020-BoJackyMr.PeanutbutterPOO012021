97ecd46657f80ddf229165eabebc2c805330ec36
00371020 <77371361+00371020@users.noreply.github.com>
2021-03-11 12:41:16 -0600

    Modify: ProgramaLab01Clan

--- ProgramaLab01Clan/Program.cs
+++ ProgramaLab01Clan/Program.cs
@@ -15,12 +15,12 @@ namespace ProgramaLab01Clan
             Console.WriteLine("Enter a value for b");
             b = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)}");
+            Console.WriteLine($"Substracting {a} - {b} equals {SubstractNumbers(a, b)}");
         }
 
-        public static int AddNumbers(int a, int b)
+        public static int SubstractNumbers(int a, int b)
         {
-            return a + b;
+            return a - b;
         }
     }
 }
